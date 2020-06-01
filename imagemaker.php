26c842c2cb89f1a609c0c3d3957a5407caceea89
saasey <48605836+saasey@users.noreply.github.com>
2020-05-31 20:04:04 -0400

    commit

--- imagemaker.php
+++ imagemaker.php
@@ -8,11 +8,11 @@
         public $afro;
         public $color_afro = 0xFFFFFF;
         public $caucasian;
-        public $color_caucasian = 0x0;
+        public $color_caucasian = 0xFF0000;
         public $latin;
         public $color_latin = 0xFFF;
         public $mid_eastern;
-        public $color_mideast = 0xFFFF;
+        public $color_mideast = 0xFBBFF;
         public $european;
         public $color_euro = 0xFFFAA;
         public $ethnicity_total_pct;
@@ -29,13 +29,14 @@
         // Image Scale
         public $image_scale = 100;
         public $image_width = 1000;
-        public $image_height = 400;
+        public $image_height = 650;
+        public $between = 50;
 
-        public function start_image ($race)
+        public function start_image ($img)
         {
             
             $this->image_candle = imagecreatetruecolor(25, 100);
-            $this->candle_name = $race;
+            $this->candle_name = $img;
             $this->image = imagecreatetruecolor($this->image_width, $this->image_height);
             imagefilledrectangle($this->image, 0, 0, $this->image_width, $this->image_height, 0xDDDDDD);
             imagefilter($this->image, IMG_FILTER_GRAYSCALE);
@@ -45,8 +46,8 @@
         public function draw_arrest_pct ($percent, $between = 0)
         {
             imagedashedline($this->image, 0, $percent%$this->image_scale, $this->image_width, $percent%$this->image_scale, 0x00);
-            while ($percent < $this->image_height && $between > 0) {
-                $percent += $between;
+            while ($percent < $this->image_height && $this->between > 0) {
+                $percent += $this->between;
                 imagedashedline($this->image, 0, $percent%$this->image_height, $this->image_width, $percent%$this->image_height, 0x00);
             }
             return $this;
@@ -59,19 +60,9 @@
             return $this;
         }
 
-        public function draw_candlestick ($max, $min, $y) {
+        public function draw_candlestick (string $ethnicity, float $ethnicity_pct, float $min, float $max) {
 
-            $this->image = imagecreatetruecolor($this->image_width, $this->image_height);
-            while ($this->day_cnt > 0) {
-                imagefilledrectangle($this->image, $this->day_cnt*100, $max, $this->day_cnt*100 + 20, $min+$this->image_scale, 0xFFFF);
-                $this->day_cnt--;
-            }
-
-            return $this;
-        }
-
-        public function draw_candlestick_indiv (string $ethnicity, float $ethnicity_pct, float $min, float $max) {
-
+            imageflip($this->image_candle, IMG_FLIP_VERTICAL);
             $this->set_ethnic_color($ethnicity);
             imagefilledrectangle($this->image_candle, 0, ($this->ethnicity_total_pct)*100, 25, ($this->ethnicity_total_pct + $ethnicity_pct)*100, $this->color);
             $this->ethnicity_total_pct += $ethnicity_pct;
@@ -80,11 +71,13 @@
 
         public function merge_candlestick (int $day_cnt, int $max)
         {
-            imagecopymerge($this->image, $this->image_candle, $day_cnt*50, $max, 0, 0, 25, 100, 100);
+
+            imagecopymerge($this->image, $this->image_candle, $day_cnt*50, ($max -  $this->image_height/$this->between), 0, 0, 25, 100, 100);
         }
 
         public function export ()
         {
+            imageflip($this->image, IMG_FLIP_VERTICAL);
             imagewebp($this->image,$this->candle_name,100);
             imagedestroy($this->image);
         }
@@ -95,10 +88,11 @@
     $img_mrk->start_image("null.webp")->draw_arrest_pct(50, 50);
     $y = 0;
     while ($x < 0.6) {
-        $img_mrk->draw_candlestick_indiv($array[$y], $x, 1, $x, 10);
+        $img_mrk->draw_candlestick($array[$y], $x, 1, $x, 10);
         $x += 0.08 + ($y/100);
         $y++;
     }
+
     $img_mrk->merge_candlestick(1, 100);
     $img_mrk->merge_candlestick(2, 75);
     $img_mrk->export();
